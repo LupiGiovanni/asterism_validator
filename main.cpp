830217d9181753b7a1b6298a59e553e3cb8c0008
giovannilupi <giovanni.lupi1@edu.unifi.it>
2025-12-16 11:00:20 +0100

    Main.cpp changes

--- main.cpp
+++ main.cpp
@@ -9,30 +9,34 @@ int main() {
     Board board_2 (Board_type::type_2);
     Board board_3 (Board_type::type_3);
 
-    int total = 0;
-    int valid = 0;
-    int not_valid = 0;
+    int count_total = 0;
+    int count_valid = 0;
+    int count_not_valid = 0;
     std::vector<Asterism> asterisms_not_valid {};
+    std::vector<Asterism> asterisms_valid;
 
     for (auto asterism = dataset.begin(); asterism != dataset.end(); ++asterism) {
-        total++;
+        count_total++;
         if (asterism->is_valid(board_1, board_2, board_3)) {
-            valid++;
-            std::cout << "Valid" << std::endl;
+            count_valid++;
+            asterisms_valid.push_back(*asterism);
         }
         else {
-            not_valid++;
+            count_not_valid++;
             asterisms_not_valid.push_back(*asterism);
-            std::cout << "Not valid";
         }
     }
 
     std::cout << "---------------------------------" << std::endl;
-    std::cout << "Total NGS: " << total << std::endl;
-    std::cout << "Valid NGSs: " << valid << std::endl;
-    std::cout << "Not valid NGSs: " << not_valid << std::endl;
+    std::cout << "Total NGS: " << count_total << std::endl;
+    std::cout << "Valid NGSs: " << count_valid << std::endl;
+    std::cout << "Not valid NGSs: " << count_not_valid << std::endl;
 
     for (auto asterism = asterisms_not_valid.begin(); asterism != asterisms_not_valid.end(); ++asterism) {
+        draw(*asterism, board_1, board_2, board_3);
+    }
+
+    for (auto asterism = asterisms_valid.begin(); asterism != asterisms_valid.end(); ++asterism) {
         draw(*asterism, board_1, board_2, board_3);
     }
 }
