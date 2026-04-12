57cb51eb0adedeea0715e135de72af031101cffa
giovannilupi <giovanni.lupi1@edu.unifi.it>
2026-04-12 09:15:47 +0200

    minor things

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ int main () {
     Graphic_viewer gv;
     Dataset_generator dg;
 
-    Simulation_manager::simulate_dataset(Movement::A_star, cedric_dataset_extended);
 
     // dg.generate_dataset();
     // std::vector<Asterism> hard_mode_dataset = dg.get_dataset();
     //
-    // for (int i = 1; i < hard_mode_dataset.size(); ++i) {
+    // for (int i = 1; i < hard_mode_dataset.size(); ++i)
     //     gv.animate(Movement::A_star, hard_mode_dataset[i-1], hard_mode_dataset[i]);
-    // }
 
+
+    for (int i = 1; i < cedric_dataset.size(); ++i)
+        gv.animate(Movement::A_star, cedric_dataset[i-1], cedric_dataset[i]);
 
 
     return 0;
